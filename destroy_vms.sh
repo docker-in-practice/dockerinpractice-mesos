b6df3aededb7b97dfe5440cc2f658fa33ff57042
Ian Miell <ian.miell@gmail.com>
2016-05-18 09:19:09 +0100

    dvm

--- destroy_vms.sh
+++ destroy_vms.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-while true 
-do
-	VBoxManage list runningvms | grep dockerinpractice_mesos | awk '{print $1}' | xargs --no-run-if-empty -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | awk '{print $1}'  | xargs --no-run-if-empty -IXXX VBoxManage unregistervm 'XXX' --delete
-	if [[ $(VBoxManage list vms | wc -l) == '0' ]]
-	then
-		break
-	else
-		ps -ef | grep virtualbox | grep dockerinpractice_mesos | awk '{print $2}' | xargs kill
-		sleep 10
-	fi
-done
+if [[ $(command -v VBoxManage) != '' ]]
+then
+	while true 
+	do
+		VBoxManage list runningvms | grep {{ skeleton.module_name }} | awk '{print $1}' | xargs -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | grep {{ skeleton.module_name }} | awk '{print $1}'  | xargs -IXXX VBoxManage unregistervm 'XXX' --delete
+		if [[ $(VBoxManage list vms | grep {{ skeleton.module_name }} | wc -l) -eq '0' ]]
+		then
+			break
+		else
+			ps -ef | grep virtualbox | grep {{ skeleton.module_name }} | awk '{print $2}' | xargs kill
+			sleep 10
+		fi
+	done
+fi
+
